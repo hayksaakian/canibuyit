42b16acbc976a8c7a42ad3744a09229d8087aaa1
hayksaakian <fakest.id.ever@gmail.com>
2012-11-16 12:30:38 -0800

    my bad

--- app/models/product.rb
+++ app/models/product.rb
@@ -26,6 +26,7 @@ class Product
   end
 
   def email
+  	require 'rest_client'
   	API_KEY = ENV['MAILGUN_API_KEY']
 		API_URL = "https://api:#{API_KEY}@api.mailgun.net/v2/mailgun.net"
 		RestClient.post API_URL+"/messages", 
