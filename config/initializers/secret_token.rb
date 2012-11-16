ec3f5e2eed2e4a19e2acdb90f35ad6b5d49c1aff
hayksaakian <fakest.id.ever@gmail.com>
2012-11-16 11:30:34 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Canibuyit::Application.config.secret_token = '641b4d5d019a1b589df9f3d5ddc70d8f39575ca492c548024e1546b837a2532bccc1cf316d61f45a5442dda31a2c6a6d6a2b2797a9b9bfaaee651dee5feae129'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Canibuyit::Application.config.session_store :cookie_store, key: '_canibuyit_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Canibuyit::Application.config.session_store :active_record_store
